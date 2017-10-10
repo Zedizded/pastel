b161657b73e5c140115f100968423d31ab79c23e
Zedizded <zedmatt@live.fr>
2017-10-10 16:05:40 +0200

    Article/Edition - Vue et controller simple

--- src/Pastel/PlatformBundle/Controller/PlatformController.php
+++ src/Pastel/PlatformBundle/Controller/PlatformController.php
@@ -4,18 +4,25 @@ namespace Pastel\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Pastel\PlatformBundle\Entity\Article;
 use Pastel\PlatformBundle\Entity\User;
 use Pastel\PlatformBundle\Form\ArticleType;
 
 class PlatformController extends Controller
-{
+{	
+	/**
+     * @Route("/", name="pastel_platform_homepage")
+     */
 	public function indexAction()
 	{
 		return $this->render('PastelPlatformBundle:Default:index.html.twig');
 	}
 
-	public function creationAction(Request $request)
+	/**
+     * @Route("/creation", name="pastel_platform_creation")
+     */
+    public function creationAction(Request $request)
 	{
 		$article = new Article();
 		$form   = $this->get('form.factory')->create(ArticleType::class, $article);
@@ -38,4 +45,28 @@ class PlatformController extends Controller
 			'form' => $form->createView(),
 			));
 	}
+    
+	/**
+     * @Route("/edition/{id}", name="pastel_platform_edition")
+     */
+	public function editionAction(Request $request,article $article, $id)
+	{
+		
+		$form   = $this->get('form.factory')->create(ArticleType::class, $article);
+
+		if ($request->isMethod('POST')) {
+			
+			$form->handleRequest($request);
+
+			if ($form->isValid()) {
+
+				$em = $this->getDoctrine()->getManager();
+				$em->flush();
+			}
+		}
+
+		return $this->render('PastelPlatformBundle:Default:edition.html.twig', array(
+			'form' => $form->createView(),
+			));
+	}    
 }
